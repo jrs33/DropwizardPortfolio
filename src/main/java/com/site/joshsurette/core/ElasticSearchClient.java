208b6f76352815e72f25dbc4d1ba2eeb096dd261
Josh <surettej@bu.edu>
2018-09-26 23:14:46 -0400

    extra error handling

--- src/main/java/com/site/joshsurette/core/ElasticSearchClient.java
+++ src/main/java/com/site/joshsurette/core/ElasticSearchClient.java
@@ -6,10 +6,15 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class ElasticSearchClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticSearchClient.class);
+    private static final SearchHits EMPTY_HITS = SearchHits.empty();
 
     private final RestHighLevelClient restHighLevelClient;
 
@@ -19,7 +24,12 @@ public class ElasticSearchClient {
         this.restHighLevelClient = restHighLevelClient;
     }
 
-    public SearchHits search(String[] indices) throws IOException {
+    public SearchHits search(final String[] indices) throws IOException {
+        if(!isValidIndiceArgument(indices)) {
+            logger.error("invalid indices passed returning empty hits: " + indices);
+            return EMPTY_HITS;
+        }
+
         SearchRequest searchRequest = new SearchRequest(indices);
         SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
         restHighLevelClient.close();
@@ -27,11 +37,20 @@ public class ElasticSearchClient {
         return searchResponse.getHits();
     }
 
-    public SearchHits search(String[] indices, SearchSourceBuilder searchSourceBuilder) throws IOException {
+    public SearchHits search(final String[] indices, final SearchSourceBuilder searchSourceBuilder) throws IOException {
+        if(!isValidIndiceArgument(indices)) {
+            logger.error("invalid indices passed returning empty hits: " + indices);
+            return EMPTY_HITS;
+        }
+
         SearchRequest searchRequest = new SearchRequest(indices, searchSourceBuilder);
         SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
         restHighLevelClient.close();
 
         return searchResponse.getHits();
+    }
+
+    private static boolean isValidIndiceArgument(final String[] indices) {
+        return indices.length > 0;
     }
 }
